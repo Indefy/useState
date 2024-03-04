c30076574d53cb028fd737d70296256de389484c
Adi Booker <adib4324@gmail.com>
2024-03-04 20:23:26 +0200

    fix bonus

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div className="app">
       <TopBar>React Components are state machines</TopBar>
-      <h1 className="headline white-text">Stateful Bookstore  </h1>
+      <h1 className="headline white-text">{'<<<< Stateful Bookstore >>>>'}</h1>
       <div className="hbox space-between mt20">
       <Books />
       </div>
